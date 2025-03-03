bac595b05e64cf058b552b5b2edbf59f50fe4179
ahmed abbassi <abbassia082@gmail.com>
2025-03-03 17:28:32 +0100

    added ui for login and signin

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+const MONGO_URI= process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+console.log("Server running on port:", PORT);
+
 const app = express();
-
-
-
-//username : abbassia082
-//password : WmQyW1i2EdGUZLZh
+mongoose.connect(MONGO_URI).then(()=>{
+    console.log("connected to database")
+    app.listen(PORT, () => {
+        console.log('Server is running on port ', PORT);
+    });
+}).catch((err)=>{
+    console.log(err)
+})
+app.get('/', (req, res) => {    
+    res.send('Hello World');
+});
