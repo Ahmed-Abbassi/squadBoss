580bcb3272c2349edb4b1d2d34d83bde9b74e2c9
yassine <hassyassine6@gmail.com>
2025-03-10 14:13:58 +0100

    delete backend

--- backend/models/team.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const teamSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  coach: { type: mongoose.Schema.Types.ObjectId, ref: 'coach' },
-  players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'player' }]
-});
-
-module.exports = mongoose.model('team', teamSchema); 
